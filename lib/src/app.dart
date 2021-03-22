9309ff38accd81413b611083675b74a1e5c5d811
9sorn <kaisorn661600@gmail.com>
2021-03-22 11:17:10 +0700

    stucture

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:my_stock/src/pages/home/home_page.dart';
 
 class App extends StatelessWidget {
   @override
@@ -14,51 +15,3 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Kaisorn',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}

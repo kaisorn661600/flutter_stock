a8360aac50d73afbb9c635b8a77c8b0faddb152e
9sorn <kaisorn661600@gmail.com>
2021-03-23 08:41:36 +0700

    login page

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -7,16 +7,92 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         fit: StackFit.expand,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text('Header'),
-              Text('login'),
-              Text('login button'),
-              Text('forgot password'),
-              Text('sso'),
-            ],
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    //Colors.white,
+                    Color(0XFFffffff),
+                    Color(0XFFFFA726)
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [0.5, 1.0]),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 75),
+                Image.network(
+                    'https://d2f7anuvnar8n5.cloudfront.net/p/assets/animations/hurricane_1e8e060c71635d9c72b776820991d419.png',),
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Card(
+                      margin: EdgeInsets.only(
+                        bottom: 22,
+                        left: 22,
+                        right: 22,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 22,
+                          right: 22,
+                          top: 22,
+                          bottom: 42,
+                        ),
+                        child: Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: "exmaple@gmail.com",
+                                labelText: "Username",
+                                icon: Icon(Icons.person),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                            Divider(
+                              height: 28,
+                              indent: 22,
+                              endIndent: 22,
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                labelText: "Password",
+                                icon: Icon(Icons.lock),
+                                border: InputBorder.none,
+                              ),
+                              obscureText: true,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 280,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('login click!!!');
+                        },
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Text('login button'),
+                Text('forgot password'),
+                Text('sso'),
+              ],
+            ),
           ),
         ],
       ),

d2059b73109f7b8d796bc00c6529c41a0045eeb8
9sorn <kaisorn661600@gmail.com>
2021-03-23 15:34:00 +0700

    Add Navigation Bar

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_stock/src/configs/app_route.dart';
+import 'package:my_stock/src/pages/login/background_theme.dart';
+import 'package:my_stock/src/view_models/menu_viewmodel.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-
-
-  final String title;
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -23,8 +20,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: CommonDrawer(),
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Home page"),
       ),
       body: Center(
         child: Column(
@@ -44,6 +42,57 @@ class _HomePageState extends State<HomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CommonDrawer extends StatelessWidget {
+  const CommonDrawer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('kaisorn.dev'),
+            accountEmail: Text('kaisorn@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/50194756?s=460&u=c5a19b9b2fb0fff0f46bcdb96a36eb81d1e1accf&v=4'),
+            ),
+            decoration: BoxDecoration(
+              gradient: BackGroundTheme.gradient,
+            ),
+          ),
+          ...MenuViewModel()
+              .items
+              .map(
+                (e) => ListTile(
+                  onTap: () {
+                    e.onTap(context);
+                  },
+                  leading: Icon(
+                    e.icon,
+                    color: e.iconColor,
+                  ),
+                  title: Text(e.title),
+                ),
+              )
+              .toList(),
+          Spacer(),
+          ListTile(
+            onTap: () {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, AppRoute.loginRoute, (route) => false);
+            },
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+          ),
+        ],
       ),
     );
   }
